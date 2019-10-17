6918348cc98d21578c4c483ad0d7fcef6a99b871
Matthew Truelove <mgtruelove@hotmail.com>
2019-10-17 16:29:24 -0400

    Instructor and Tool Models

--- Final_Project/src/main/java/com/collabera/finalProject/model/Address.java
+++ Final_Project/src/main/java/com/collabera/finalProject/model/Address.java
@@ -7,16 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "address")
 public class Address implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
+	@Column(name = "id", updatable = false, nullable = false)
 	private Long id;
 	private String number; // street number
 	private String name; // street name
