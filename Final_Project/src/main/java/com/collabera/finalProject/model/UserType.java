01f50b15a939941fedf52a115c05d7621ad87783
[Rebecca Hull] <[RLHull@my.harrisburgu.edu]>
2019-10-17 16:31:47 -0400

    Added User, UserType, Client

--- Final_Project/src/main/java/com/collabera/finalProject/model/UserType.java
+++ Final_Project/src/main/java/com/collabera/finalProject/model/UserType.java
@@ -1,8 +1,39 @@
 package com.collabera.finalProject.model;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
-public class UserType {
+public class UserType implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+	private String name;
+	public UserType(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
