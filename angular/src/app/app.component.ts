07be6c656f61921eaf9073f6f4b8babdc4fa6f32
Omran Losinno <olosinno@comcast.net>
2019-10-21 15:09:13 -0400

    Added get(s) to app.component.ts

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Repository } from './models/repository';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'Collabera Web Development';
+  constructor(private repo: repository) {}//.ts file is currently empty
+
+  get address(): Address {
+    this.repo.address; }
+
+  get client(): Client {
+    this.repo.address; }
+
+  get instructor(): Instructor {
+    this.repo.address; }
+
+  get location(): Location {
+    this.repo.address; }
+
+  get resource(): Resource {
+    this.repo.address; }
+
+  get student(): Student {
+    this.repo.address; }
+
+  get tool(): Tool {
+    this.repo.address; }
+
+  get user(): User {
+    this.repo.address; }
+
+    get usertype(): UserType {
+      this.repo.address; }
+// Have yet to do create, replace, update, or delete
 }
