d71cea0889ed20d4b6eab9005b8cf267b15b19e0
[Rebecca Hull] <[RLHull@my.harrisburgu.edu]>
2019-10-21 14:59:47 -0400

    Did some stuff

---
+++ angular/src/app/View/userLogin.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+    selector: 'user-login',
+    templateUrl: 'userLogin.component.html'
+})
+export class UserLoginComponent {
+
+}
