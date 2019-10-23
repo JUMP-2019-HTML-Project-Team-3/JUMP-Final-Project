fb937cfb0ab4cb6f0e62e1e0be9c95dfc5f4030c
Matthew Truelove <mgtruelove@hotmail.com>
2019-10-23 09:45:53 -0400

    angular set up

--- angular/src/app/View/InstructorLoggedIn/instructor.module.ts
+++ angular/src/app/View/InstructorLoggedIn/instructor.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser"
 import { RouterModule } from "@angular/router";
 import { FormsModule } from "@angular/forms";
+import { InstructorHomeComponent } from "./instructorHome.component";
 
 @NgModule({
     imports: [BrowserModule, RouterModule, FormsModule],
     declarations: [
-        
+      InstructorHomeComponent
     ]
 })
 export class InstructorModule { }
