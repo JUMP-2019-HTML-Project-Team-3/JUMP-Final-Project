802903c2d805b972eac3257947e39bd4999bcaef
Matthew Truelove <mgtruelove@hotmail.com>
2019-10-24 14:13:50 -0400

    angular added card modules and components

---
+++ angular/src/app/View/cards/cardCarousel.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-cardCarousel',
+    templateUrl: './cardCarousel.component.html'
+})
+
+export class CardCarouselComponent {
+
+}
