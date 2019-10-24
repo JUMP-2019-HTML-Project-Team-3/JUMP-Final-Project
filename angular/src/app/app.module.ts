238499140290093f352c513a46e6a84ab7c601d2
Matthew Truelove <mgtruelove@hotmail.com>
2019-10-24 11:00:59 -0400

    angular routing fixes

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,28 +1,18 @@
-import { RoutingConfig } from './app.routing';
-import { BrowserModule } from '@angular/platform-browser';
+// Angular Modules
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { RouterModule, Routes } from '@angular/router';
+import { RoutingConfig } from './app.routing';
+// Custom Module Imports
 import { InstructorModule } from './View/InstructorLoggedIn/instructor.module';
+// Component Imports
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import {UserLoginComponent} from './View/userLogin.component';
-import {ReactiveFormsModule} from '@angular/forms';
-
-import {RouterModule, Routes} from '@angular/router';
 import { LogoutComponent } from './logout/logout.component';
 import { CheckComponent } from './check/check.component';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {User} from './models/user.model';
-import {Student} from './models/student.model';
-import { InstructorHomeComponent } from './View/InstructorLoggedIn/instructorHome.component';
-
-const appRoutes: Routes = [
-  { path: '', redirectTo: '/instructor', pathMatch: 'full' },
-  { path: 'instructor', component: InstructorHomeComponent},
-  { path: 'student', component: Student },
-  { path: 'login', component: UserLoginComponent },
-  { path: 'logout', component: LogoutComponent },
-  { path: 'check', component: CheckComponent }
-];
 
 @NgModule({
   schemas: [
@@ -33,15 +23,13 @@ const appRoutes: Routes = [
     NavbarComponent,
     UserLoginComponent,
     LogoutComponent,
-    CheckComponent,
-    InstructorHomeComponent
+    CheckComponent
   ],
   imports: [
     RoutingConfig,
     BrowserModule,
     ReactiveFormsModule,
     InstructorModule,
-    RouterModule.forRoot(appRoutes),
     HttpClientModule
   ],
   providers: [
