47443d651dbbdafbc4cca9757aa1cbdc94696106
Omran Losinno <olosinno@comcast.net>
2019-10-30 16:45:07 -0400

    Fixed flaws in Instructor Controller and Service

--- Final_Project/src/main/java/com/collabera/finalProject/service/InstructorService.java
+++ Final_Project/src/main/java/com/collabera/finalProject/service/InstructorService.java
@@ -21,14 +21,14 @@ public class InstructorService {
 
 	@Autowired
 	private InstructorRepository instructorRepository;
-	
+
 	//Constructor
 	public InstructorService(InstructorRepository instructorRepository)
 	{
 		this.instructorRepository = instructorRepository;
 	}
-	
-	// Create Instructor
+
+	//Create Instructor
 	public void addInstructor(String firstName, String lastName, String description, UserType userType, Long long1)
 	{
 		Instructor tempInstructor = new Instructor();
@@ -37,9 +37,22 @@ public class InstructorService {
 		tempInstructor.setLastName(lastName);
 		tempInstructor.setDescription(description);
 		tempInstructor.setUserType(userType);
-		instructorRepository. save(tempInstructor);
+
+		instructorRepository.save(tempInstructor);
 	}
-	
+
+	//Find by Id
+	public Optional<Instructor> getInstructorById(Long id)
+	{
+		return instructorRepository.findById(id);
+	}
+
+	//Find All
+	public List<Instructor> findAll()
+	{
+		return instructorRepository.findAll();
+	}
+
 	// Add Location
 	public void addLocation(Location aLocation, Instructor aInstructor)
 	{
@@ -50,67 +63,55 @@ public class InstructorService {
 		
 		instructorRepository.save(aInstructor);
 	}
-	
+
 	// Add/Remove Student
 	public void addStudent(Student s, Instructor i)
 	{
 		i.addStudent(s);
 		instructorRepository.save(i);
 	}
-	
+
 	public void removeStudent(Student s, Instructor i)
 	{
 		i.removeStudent(s);
 		instructorRepository.save(i);
 	}
-	
+
 	// Add/Remove Resource
 	public void addResource(Resource r, Instructor i)
 	{
 		i.addResource(r);
 		instructorRepository.save(i);
 	}
-	
+
 	public void removeResource(Resource r, Instructor i)
 	{
 		i.removeResource(r);
 		instructorRepository.save(i);
 	}
-	
+
 	// Add/Remove Tool
 	public void addTool(Tool t, Instructor i)
 	{
 		i.addTool(t);
 		instructorRepository.save(i);
 	}
-	
+
 	public void removeTool(Tool t, Instructor i)
 	{
 		i.removeTool(t);
 		instructorRepository.save(i);
 	}
-	
-	//Find All
-	public List<Instructor> findAll()
-	{
-		return instructorRepository.findAll();
-	}
-	
-	//Find by Id
-	public Optional<Instructor> getInstructorById(Long id)
-	{
-		return instructorRepository.findById(id);
-	}
-	
+
 	//Update
 	public void updateInstructor(Instructor instructor)
 	{
 		Optional<Instructor> findById = instructorRepository.findById(instructor.getId());
-		
+
 		if(findById.isPresent())
 		{
 			Instructor instructorToUpdate = findById.get();
-			
+
 			instructorToUpdate.setFirstName(instructor.getFirstName());
 			instructorToUpdate.setLastName(instructor.getLastName());
 			instructorToUpdate.setDescription(instructor.getDescription());
@@ -127,13 +128,11 @@ public class InstructorService {
 		{
 			throw new IllegalArgumentException();
 		}
-		
 	}
 
 	//Delete
 	public void deleteInstructor(Long id)
 	{
 		instructorRepository.deleteById(id);
-	}
-	
+	}	
 }
